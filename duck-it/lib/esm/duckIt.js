42242eab6ea65c284bfe1309d6ec7ef981718265
Utilisateur <franck.ledoux.pro@gmail.com>
2019-10-07 22:37:40 +0200

    update dep

--- duck-it/lib/esm/duckIt.js
+++ duck-it/lib/esm/duckIt.js
@@ -1,8 +1,9 @@
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
@@ -22,7 +23,7 @@ const defaultSearchOption = {
     format: 'json'
 };
 function makeQueryString(searchQuery, userOption) {
-    const option = Object.assign({}, defaultSearchOption, userOption);
+    const option = Object.assign(Object.assign({}, defaultSearchOption), userOption);
     return Object.entries(option).reduce((acc, [userKey, userValue]) => {
         const [actualKey, actualValue] = mapParameters(userKey, userValue);
         acc[actualKey] = actualValue;
